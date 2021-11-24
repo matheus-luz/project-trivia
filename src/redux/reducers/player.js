5d019042759362919a61487efc3a19d6fe22f19c
Murilo <murilocarrijoadm@gmail.com>
2021-11-24 17:12:33 -0300

    configuraçoes do redux

---
+++ src/redux/reducers/player.js
@@ -0,0 +1,15 @@
+const INICIAL_STATE = {
+  name: '',
+  assertions: 0,
+  score: 0,
+  gravatarEmail: '',
+};
+
+const player = (state = INICIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default player;
