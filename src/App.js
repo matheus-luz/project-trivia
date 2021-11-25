7e37eb2692e3e6ad1a7d54e058bd5ed4c640adc9
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-11-25 18:29:06 -0300

    Adicinando feedback

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Switch } from 'react-router';
 import './App.css';
 import Login from './page/Login';
+import Game from './page/Game';
 
 export default function App() {
   return (
     <main className="App">
       <Switch>
+        <Route path="/game" component={ Game } />
         <Route path="/" component={ Login } />
       </Switch>
     </main>
